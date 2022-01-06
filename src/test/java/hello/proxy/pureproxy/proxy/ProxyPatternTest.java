85f5c1c58a1218520a86221de2c86f33b5bd5cbb
kingchan223 <kokiyo97@naver.com>
2022-01-06 21:30:32 +0900

    프록시 패턴 예제로 적용해보기

---
+++ src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -0,0 +1,32 @@
+package hello.proxy.pureproxy.proxy;
+
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
+import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
+import hello.proxy.pureproxy.proxy.code.RealSubject;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ProxyPatternTest {
+
+    @Test
+    void noProxyTest(){
+        RealSubject realSubject = new RealSubject();
+        ProxyPatternClient proxyPatternClient = new ProxyPatternClient(realSubject);
+
+        proxyPatternClient.execute();
+        proxyPatternClient.execute();
+        proxyPatternClient.execute();
+    }
+
+    @Test
+    void cacheProxyTest(){
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient proxyPatternClient = new ProxyPatternClient(cacheProxy);
+
+        proxyPatternClient.execute();
+        proxyPatternClient.execute();
+        proxyPatternClient.execute();
+    }
+}
